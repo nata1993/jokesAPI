e1cbb0b7594b24cc2f132083d2c69309052963c7
Opilane TTHK <opilane@tthk.ee>
2020-11-11 10:28:56 +0200

    m

--- server.js
+++ server.js
@@ -13,46 +13,52 @@ const jokesSchema = {
 
 const Joke = mongoose.model('Joke', jokesSchema);
 
-
-app.get('/jokes', (req, res) => {
-    Joke.find((error, jokes) => {
-        if(!error){
-            res.send(jokes);
-        }
-        else{
-            res.send(error);
-        }
-    });
-});
-
-app.post('/jokes', (req, res) => {
-    console.log(req.body.title);
-    console.log(req.body.content);
-    let newJoke = new Joke({
-        title: req.body.title,
-        content: req.body.content
-    });
-
-    newJoke.save((error => {
-        if(!error){
-            res.send('data sent');
-        }
-        else{
-            res.send(error);
-        }
-    }));
-});
-
-app.delete('/jokes', (req, res) => {
-    Joke.deleteMany((error) => {
-        if(!error){
-            res.send('deleted all records');
-        }
-        else{
-            res.send(error);
-        }
-    });
-});
+// chained route handlers
+app.route('/jokes')
+.get(
+    
+    (req, res) => {
+        Joke.find((error, jokes) => {
+            if(!error){
+                res.send(jokes);
+            }
+            else{
+                res.send(error);
+            }
+        });
+    }
+)
+.post(
+    (req, res) => {
+        console.log(req.body.title);
+        console.log(req.body.content);
+        let newJoke = new Joke({
+            title: req.body.title,
+            content: req.body.content
+        });
+    
+        newJoke.save((error => {
+            if(!error){
+                res.send('data sent');
+            }
+            else{
+                res.send(error);
+            }
+        }));
+    }
+)
+.delete(
+    (req, res) => {
+        Joke.deleteMany((error) => {
+            if(!error){
+                res.send('deleted all records');
+            }
+            else{
+                res.send(error);
+            }
+        });
+    }
+);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
