0e2c814a264356475f333710b83c62f35c99bc6a
Opilane TTHK <opilane@tthk.ee>
2020-11-18 10:28:40 +0200

    m

--- server.js
+++ server.js
@@ -60,6 +60,48 @@ app.route('/jokes')
     }
 );
 
+
+// get specific joke
+app.route("/jokes/:jokeTitle")
+    .get((req, res) => {        // koolin tähendab, et tulevad parameetrid peale koolonit
+        Joke.findOne({title : req.params.jokeTitle}, (error, jokeFound) => {    // params.jokeTitle ongi parameeter
+            if(jokeFound){
+                res.send(jokeFound);
+            }
+            else{
+                res.send("no joke title found!");
+            }
+        });   
+    })
+    .put((req, res) => {
+        Joke.update(
+            { title: req.params.jokeTitle },    // the document update
+            { title: req.body.title, content: req.body.content},
+            {overwrite: true},
+            (error) => {
+                if(!error){
+                    res.send("document updated");
+                }
+                else{
+                    res.send(error);
+                }
+            }
+        );
+    })
+    .delete((req, res) =>{
+        Joke.deleteOne(
+            {title: req.params.jokeTitle},
+            (error)=> {
+                if(!error){
+                    res.send("document deleted");
+                }
+                else{
+                    res.send(error);
+                }
+            }
+        );
+    });
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
